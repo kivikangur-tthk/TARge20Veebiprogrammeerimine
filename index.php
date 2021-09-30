eb0ab40ddcc68b5b36f6f2c3ce5019f9342dc0ef
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2021-09-30 10:00:14 +0300

    Lisa link harjutused kaustale

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 echo "See fail peabki veateate andma!<br>\n";
 echo "\tTeisel real olev echo<br>\n";
 ?>
-    Hello Error World!<br>
+Hello Error World!<br>
 <?php
 echo "Veel midagi<br>\n";
+?>
+
+<a href="harjutused/">Lahendatud Harjutused</a><br>
